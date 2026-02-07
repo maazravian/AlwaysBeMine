fa135ee5fac0a317fd57a18787f4aa306eeba1be
Maaz Javed <maaz.ravian@gmail.com>
2026-02-07 18:46:52 +0100

    Update Swal title message in App component

--- src/App.jsx
+++ src/App.jsx
@@ -247,7 +247,7 @@ export default function Page() {
   useEffect(() => {
     if (yesPressed && noCount < 4 && !popupShown) {
       Swal.fire({
-        title: "I love you sooo Much!!!❤️, You’ve stolen my heart completely!!! 🥰💖 But itni pyaari ladki aur itni jaldi haan? Thoda aur nakhre karke mujhe tarpaao na! 🥰✨",
+        title: "I love you sooo Much!!!❤️, then lets make it official now on 27th march IA 💖🥰✨",
         showClass: {
           popup: `
             animate__animated
